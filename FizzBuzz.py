1ef99713990acd110ab3857be86a1902175afa22
Jimmy <jdsckz@umsystem.edu>
2023-03-09 14:59:15 -0600

    made code more understandable

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -1,20 +1,34 @@
+#FizzBuzz Program to count from 1-100
+#Replaces numbers divisible by 3 with "Fizz"
+#Replaces numbers divisible by 5 with "Buzz"
+#Replaces numbers divisble by 3 & 5 with "FizzBuzz"
+#Prints all other numbers in range
+
+#function to count from 1-100 and replace numbers with appropriate words
 def fizzbuzz():
-    x = range(1,101)
-    for i in x:
-        if i%3==0 and i%5==0:
+    #This will count from 1-100
+    count = range(1,101)
+    #Loop through the numbers
+    for number in count:
+        #if divisble by 3 and 5, print "FizzBuzz"
+        if number%3==0 and number%5==0:
             print("FizzBuzz")
             continue
-        if i%3==0:
+        #if divisible by 3, print "Fizz"
+        if number%3==0:
             print("Fizz")
             continue
-        if i%5==0:
+        #if divisible by 5, print "Buzz"
+        if number%5==0:
             print("Buzz")
             continue
+        #if not divisible by 3 or 5, print the number
         else:
-            print(i)
-
+            print(number)
+#Don't forget to define main
 def main():
     fizzbuzz()
 
+#if __name__ == __main__, execute main()
 if __name__ == "__main__":
     main()
