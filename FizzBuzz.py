65274921ea13fadafda6e64ef906dadbebb48139
Jimmy <jdsckz@umsystem.edu>
2023-03-09 14:45:41 -0600

    Improved efficiency of fizzbuzz.py

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -1,13 +1,20 @@
-x = range(1,101)
-for i in x:
-    if i%3==0 and i%5==0:
-        print("FizzBuzz")
-        continue
-    if i%3==0:
-        print("Fizz")
-        continue
-    if i%5==0:
-        print("Buzz")
-        continue
-    else:
-        print(i)
+def fizzbuzz():
+    x = range(1,101)
+    for i in x:
+        if i%3==0 and i%5==0:
+            print("FizzBuzz")
+            continue
+        if i%3==0:
+            print("Fizz")
+            continue
+        if i%5==0:
+            print("Buzz")
+            continue
+        else:
+            print(i)
+
+def main():
+    fizzbuzz()
+
+if __name__ == "__main__":
+    main()
